a4d2c06f5d038722652d5d056079b365d26cac44
FilipRosiak1 <148724989+FilipRosiak1@users.noreply.github.com>
2024-03-15 02:45:36 +0100

    Add files via upload

---
+++ all_questions.py
@@ -0,0 +1,14 @@
+questions = [
+    {"question": "Kto jest autorem powieści 'Zbrodnia i kara'?", "options": ["Fiodor Dostojewski", "Leo Tolstoj", "Gabriel Garcia Marquez", "Herman Melville"], "correct_answer": "Fiodor Dostojewski"},
+    {"question": "Ile planet znajduje się w naszym Układzie Słonecznym?", "options": ["7", "8", "9", "10"], "correct_answer": "8"},
+    {"question": "Jak nazywa się największa wyspa na świecie?", "options": ["Australia", "Borneo", "Nowa Gwinea", "Grenlandia"], "correct_answer": "Grenlandia"},
+    {"question": "Kto napisał 'Romeo i Julia'?", "options": ["William Shakespeare", "Jane Austen", "Charles Dickens", "Emily Brontë"], "correct_answer": "William Shakespeare"},
+    {"question": "Jaka jest stolica Japonii?", "options": ["Pekin", "Tokio", "Seul", "Osaka"], "correct_answer": "Tokio"},
+    {"question": "Ile kontynentów jest na świecie?", "options": ["5", "6", "7", "8"], "correct_answer": "7"},
+    {"question": "Kto był pierwszym prezydentem Stanów Zjednoczonych?", "options": ["George Washington", "Thomas Jefferson", "John Adams", "Abraham Lincoln"], "correct_answer": "George Washington"},
+    {"question": "Ile stron ma sześciokąt?", "options": ["4", "5", "6", "8"], "correct_answer": "6"},
+    {"question": "Która planeta jest nazywana 'Wieczorną Gwiazdą'?", "options": ["Mars", "Jowisz", "Wenus", "Saturn"], "correct_answer": "Wenus"},
+    {"question": "Które państwo jest największe pod względem powierzchni?", "options": ["Rosja", "Kanada", "Chiny", "Stany Zjednoczone"], "correct_answer": "Rosja"},
+    {"question": "Które morze leży między Europą a Afryką?", "options": ["Morze Śródziemne", "Morze Czarne", "Morze Egejskie", "Morze Adriatyckie"], "correct_answer": "Morze Śródziemne"},
+    {"question": "Kto jest autorem malarstwa 'Mona Lisa'?", "options": ["Vincent van Gogh", "Leonardo da Vinci", "Pablo Picasso", "Michelangelo"], "correct_answer": "Leonardo da Vinci"}
+]
